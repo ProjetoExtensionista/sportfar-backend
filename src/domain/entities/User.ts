73e149d2dbd6b3e3104581390cd52cab619d98a8
Otávio Henrique <otaviocapo@gmail.com>
2025-05-15 22:15:58 -0300

    #13 chore(Modalidades): Adicionado ApiProperty nas entities

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,27 +1,49 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('USERS')
 export class User {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
+
   @Column({ name: 'full_name' })
+  @ApiProperty()
   fullName: string;
+
   @Column()
+  @ApiProperty()
   cpf: string;
+
   @Column({ name: 'birth_date' })
+  @ApiProperty()
   birthDate: Date;
+
   @Column()
+  @ApiProperty()
   email: string;
+
   @Column()
+  @ApiProperty()
   password: string;
+
   @Column({ name: 'password_temp' })
+  @ApiProperty()
   passwordTemp: boolean;
+
   @Column()
+  @ApiProperty()
   address: string;
+
   @Column()
+  @ApiProperty()
   city: string;
+
   @Column()
+  @ApiProperty()
   cep: string;
+
   @Column()
+  @ApiProperty()
   phone: string;
 }
