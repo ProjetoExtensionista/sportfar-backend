849b197c6e91660af5f3a48f159ef465c9ad72c8
Icke758 <joseh.noal@gmail.com>
2025-06-04 20:18:39 -0300

    feat-refactor(Adicionando POST para professores - Traduzindo o nome da modalidade para inglês - Ajustando o casing das models

--- src/api/modalidades/models/createCourse.dto.ts
+++ src/api/modalidades/models/createCourse.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class CreateModalidadeDto {
+export class CreateCourseDto {
   @ApiProperty()
   name: string;
   @ApiProperty()
---
+++ src/api/users/models/userRequestDto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserRequestDto {
+  @ApiProperty()
+  fullName: string;
+  @ApiProperty()
+  cpf: string;
+  @ApiProperty()
+  birthDate: Date;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  password: string;
+  @ApiProperty()
+  passwordTemp: boolean;
+  @ApiProperty()
+  address: string;
+  @ApiProperty()
+  city: string;
+  @ApiProperty()
+  cep: string;
+  @ApiProperty()
+  phone: string;
+}
