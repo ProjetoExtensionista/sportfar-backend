426c9a28ac1d1758a892181d485ea436fed0de79
Amanda Sironi <amandasironi.contato@gmail.com>
2025-06-06 21:15:28 -0300

    #feature: crud das classes

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,18 +2,20 @@ import { Module } from '@nestjs/common';
 import { AbsenceModule } from './absence/absence.module';
 import { AbsenceTypeModule } from './absenceType/absencetype.module';
 import { AuthModule } from './auth/auth.module';
-import { ModalidadeModule } from './modalidades/course.module';
+import { ClassModule } from './classes/class.module';
+import { CourseModule } from './courses/course.module';
 import { UsersModule } from './users/users.module';
 import { UserTypeModule } from './userType/userType.module';
 
 @Module({
   imports: [
-    AuthModule,
-    UsersModule,
-    ModalidadeModule,
-    UserTypeModule,
     AbsenceModule,
     AbsenceTypeModule,
+    AuthModule,
+    ClassModule,
+    CourseModule,
+    UsersModule,
+    UserTypeModule,
   ],
 })
 export class ApiModule {}
