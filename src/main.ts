8a06c2ab442392204124aef6a9e292b024a89ac4
Otávio Henrique <otaviocapo@gmail.com>
2025-06-13 19:57:52 -0300

    feat(api+static): Adicionado prefixo global e hospedagem de arquivos estáticos

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { installSwagger } from './installers/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   installSwagger(app);
   installCORS(app);
